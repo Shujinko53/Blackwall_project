27e687c49c7d0684c4a9e516ad2ab380a64cd371
Shujinko53 <shujinko53@yandex.ru>
2021-08-21 22:24:43 +0300

    some fixes

--- src/js/script.js
+++ src/js/script.js
@@ -1,8 +1,16 @@
 const menu = document.querySelector('.header__nav'),
+      menuClose = document.querySelector('.close'),
       hamburger = document.querySelector('.hamburger');
 
+menuClose.addEventListener('click', () => {
+    if (menu.classList.contains('header__nav--active')) {
+        menu.classList.remove('header__nav--active');
+        hamburger.classList.remove('hamburger--open');
+    }
+})
 
 hamburger.addEventListener('click', () => {
     menu.classList.toggle('header__nav--active');
     hamburger.classList.toggle('hamburger--open');
 });
+
